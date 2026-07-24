030d0c74916bedf62627b3c4678118b0fa6881fb
underd0se <baris.erdogan@gmail.com>
2026-07-24 11:47:18 +0300

    release: bump version to v8.1.1-sz.1.0.1

--- firewall.sh
+++ firewall.sh
@@ -10,7 +10,7 @@
 #                                                                                                           #
 #                                 Router Firewall And Security Enhancements                                 #
 #                      By Adamm (Forked by underd0se) -  https://github.com/underd0se/Skynet-Zero           #
-#                                    24/07/2026 - v8.1.1-sz.1 (Zero Swap)                                   #
+#                                   24/07/2026 - v8.1.1-sz.1.0.1 (Zero Swap)                                  #
 #############################################################################################################
 # amtm NoMD5check
 
